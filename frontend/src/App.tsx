8c591317a8622df81ffe36575c953e91e74f0f93
Nicholas Wang <nicholaswang03@gmail.com>
2025-06-10 18:57:59 -0700

    implement sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,19 @@
 import "./App.css";
-import { setTheme } from "./utils/theme";
+import SideBar from "./SideBar";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
+      {/* todo: move title into its own component */}
       <header className="bg-amber-100 dark:bg-gray-800">
         <h1 className="text-4xl font-bold tracking-wide text-gray-900 dark:text-white sm:text-5xl md:text-6xl p-6 text-center">
           Operations Status
         </h1>
       </header>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 m-8 rounded"
-        onClick={() => {
-          setTheme("light");
-        }}
-      >
-        Light
-      </button>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 m-8 rounded"
-        onClick={() => {
-          setTheme("dark");
-        }}
-      >
-        Dark
-      </button>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 m-8 rounded"
-        onClick={() => {
-          setTheme("system");
-        }}
-      >
-        System
-      </button>
+
+      <main className="flex flex-1">
+        <SideBar />
+      </main>
     </div>
   );
 }
